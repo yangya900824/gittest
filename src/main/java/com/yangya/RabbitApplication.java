6321a86cc29ac0f8c2ef9533ddecf36420156dfc
yangya900824 <yangya900824@163.com>
2019-02-18 10:26:24 +0800

    222

--- src/main/java/com/yangya/RabbitApplication.java
+++ src/main/java/com/yangya/RabbitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitApplication {
 	/**
-	 * 
+	 * 222
 	 * @param args
 	 */
 	public static void main(String[] args) {
